eb15529907b9989c08c83b7f6dd94898142a093c
Stephen Cuppett <scuppett@redhat.com>
2020-08-26 07:09:33 -0400

    Matching file envvars to current PR in nextcloud/docker:1193

--- autoconfig/objectstorage.config.php
+++ autoconfig/objectstorage.config.php
@@ -1,18 +1,18 @@
 <?php
-if (getenv('S3_BUCKET')) {
+if (getenv('OBJECTSTORE_S3_BUCKET')) {
   $CONFIG = [
     'objectstore' => array(
         'class' => \OC\Files\ObjectStore\S3::class,
         'arguments' => array(
-            'bucket' => getenv('S3_BUCKET'),
-            'key' => getenv('S3_ACCESS_KEY') ?: '',
-            'secret' => getenv('S3_ACCESS_SECRET') ?: '',
-            'region' => getenv('S3_REGION') ?: '',
-            'hostname' => getenv('S3_HOSTNAME') ?: '',
-            'port' => getenv('S3_PORT') ?: '',
-            'autocreate' => getenv('S3_AUTOCREATE') ?: '',
-            'use_ssl' => getenv('S3_USE_SSL') ?: '',
-            'use_path_style' => getenv('S3_USE_PATH_STYLE') == true ?: false
+            'bucket' => getenv('OBJECTSTORE_S3_BUCKET'),
+            'key' => getenv('OBJECTSTORE_S3_KEY') ?: '',
+            'secret' => getenv('OBJECTSTORE_S3_SECRET') ?: '',
+            'region' => getenv('OBJECTSTORE_S3_REGION') ?: '',
+            'hostname' => getenv('OBJECTSTORE_S3_HOST') ?: '',
+            'port' => getenv('OBJECTSTORE_S3_PORT') ?: '',
+            'autocreate' => getenv('OBJECTSTORE_S3_AUTOCREATE') == true,
+            'use_ssl' => getenv('OBJECTSTORE_S3_SSL') == false ? false : true,
+            'use_path_style' => getenv('OBJECTSTORE_S3_USEPATH_STYLE') == true
          )
     )
   ];
