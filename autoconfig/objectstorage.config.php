94409ff3e0f110d75c4cb8deaabab951f2d40a01
Stephen Cuppett <scuppett@redhat.com>
2020-08-26 07:27:24 -0400

    autocreate parameter not used for S3 objectstore

--- autoconfig/objectstorage.config.php
+++ autoconfig/objectstorage.config.php
@@ -10,7 +10,6 @@ if (getenv('OBJECTSTORE_S3_BUCKET')) {
             'region' => getenv('OBJECTSTORE_S3_REGION') ?: '',
             'hostname' => getenv('OBJECTSTORE_S3_HOST') ?: '',
             'port' => getenv('OBJECTSTORE_S3_PORT') ?: '',
-            'autocreate' => getenv('OBJECTSTORE_S3_AUTOCREATE') == true,
             'use_ssl' => getenv('OBJECTSTORE_S3_SSL') == false ? false : true,
             'use_path_style' => getenv('OBJECTSTORE_S3_USEPATH_STYLE') == true
          )
